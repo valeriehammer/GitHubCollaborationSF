7d2d4613d2ac2372e5bf264c495cc569c963660e
Michelle Abt <mabt924@gmail.com>
2025-08-12 08:36:29 -0700

    Change 2

--- GitHubCollaboration/ContentView.swift
+++ GitHubCollaboration/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Change 1")
+            Text("Change 2")
         }
         .padding()
     }
