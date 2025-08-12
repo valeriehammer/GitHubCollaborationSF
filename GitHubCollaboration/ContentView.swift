e378594c8b8ac8b25243b745c3f1f463a51f1a37
Inika Bhargava <inikamehro@gmail.com>
2025-08-12 08:38:02 -0700

    change 3

--- GitHubCollaboration/ContentView.swift
+++ GitHubCollaboration/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Change 1")
             Text("Change 2")
+            Text("Change 3")
         }
         .padding()
     }
